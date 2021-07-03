bbd7225913f44c98419ca761a772de08f29ced32
YunKukPark <a01043340999@gmail.com>
2021-07-03 12:02:07 +0900

    🚧 Study about State

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import TOC from './components/TOC'
-import Subject from './components/Subject'
-import Content from './components/Content'
+import TOC from './components/TOC';
+import Subject from './components/Subject';
+import Content from './components/Content';
 import './App.css';
 
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: 'WEB', sub: 'World Wide Web' },
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"/>
-        <TOC/>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."/>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        />
+        <TOC />
+        <Content title="HTML" desc="HTML is HyperText Markup Language." />
       </div>
-    );  
+    );
   }
 }
 
