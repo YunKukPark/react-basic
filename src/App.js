d9890d11a457e34c6d068d98b67dc8d13165ce52
YunKukPark <a01043340999@gmail.com>
2021-07-03 12:46:31 +0900

    🚧 Study about Key

--- src/App.js
+++ src/App.js
@@ -9,6 +9,21 @@ class App extends Component {
     super(props);
     this.state = {
       subject: { title: 'WEB', sub: 'World Wide Web' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is HyperText...', url: '1.html' },
+        {
+          id: 2,
+          title: 'CSS',
+          desc: 'CSS is Cascading Style Sheet',
+          url: '2.html',
+        },
+        {
+          id: 3,
+          title: 'JavaScript',
+          desc: 'JS is for interactive',
+          url: '3.html',
+        },
+      ],
     };
   }
 
@@ -19,7 +34,7 @@ class App extends Component {
           title={this.state.subject.title}
           sub={this.state.subject.sub}
         />
-        <TOC />
+        <TOC data={this.state.contents} />
         <Content title="HTML" desc="HTML is HyperText Markup Language." />
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 
 class TOC extends Component {
-  render(){
-    return(
+  render() {
+    let lists = [];
+    const data = this.props.data;
+    data.map((dataItem) =>
+      lists.push(
+        <li key={dataItem.id}>
+          <a href={`/content/${dataItem.id}`}>{dataItem.title}</a>
+        </li>
+      )
+    );
+
+    return (
       <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>
+        <ul>{lists}</ul>
       </nav>
     );
   }
