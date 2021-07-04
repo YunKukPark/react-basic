093f8cfdc44b803c0ad627b0bdbbef6c46f228f7
YunKukPark <a01043340999@gmail.com>
2021-07-04 13:45:52 +0900

    🚧 Study About Event

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: 'welcome',
       subject: { title: 'WEB', sub: 'World Wide Web' },
+      welcome: { title: 'Welcom', desc: 'Hello react' },
       contents: [
         { id: 1, title: 'HTML', desc: 'HTML is HyperText...', url: '1.html' },
         {
@@ -28,14 +30,38 @@ class App extends Component {
   }
 
   render() {
+    let _title,
+      _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.content[0].title;
+      _desc = this.state.content[0].desc;
+    }
+
     return (
       <div className="App">
-        <Subject
+        {/* <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}
-        />
+        /> */}
+        <header>
+          <h1>
+            <a
+              href="/"
+              onClick={(e) => {
+                console.log(e);
+                e.preventDefault();
+              }}
+            >
+              {this.state.subject.title}
+            </a>
+          </h1>
+          {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents} />
-        <Content title="HTML" desc="HTML is HyperText Markup Language." />
+        <Content title={_title} desc={_desc} />
       </div>
     );
   }
